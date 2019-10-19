43e225c854b955ec7acc5a08f0f01c32f567378c
silvercoin <silvercoin99.999@gmail.com>
2019-10-19 10:54:56 +0900

    reactjs-books-review

---
+++ src/components/SigninForm.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const FormWrap = styled.div`
+  width: 400px;
+  margin: 0 auto;
+`;
+
+const InputArea = styled.div`
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  height: 35px;
+  border: 2px solid #fff;
+  padding: 0 10px;
+  background-color: rgba(255, 255, 255, 0.7);
+`;
+
+const ButtonArea = styled.div`
+  margin-top: 40px;
+`;
+
+const StyledButton = styled.button`
+  background-color: rgba(0, 0, 0, 0.7);
+  text-transform: uppercase;
+  border-radius: 1px;
+  border-width: 2px;
+  color: white;
+  width: 100%;
+  height: 50px;
+  border: 2px solid #fff;
+  transition: all 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    border-width: 5px;
+    background-color: pink;
+  }
+`;
+
+const LinkArea = styled.div`
+  margin-top: 15px;
+  overflow: hidden;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.7);
+  span {
+    margin: 0 5px;
+  }
+  a {
+    color: rgba(0, 0, 0, 0.7);
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+class SigninForm extends React.Component {
+  render() {
+    return (
+      <FormWrap>
+        <InputArea>
+          <StyledInput placeholder="Please enter your email" />
+        </InputArea>
+        <InputArea>
+          <StyledInput
+            placeholder="Please enter your password"
+            type="password"
+          />
+        </InputArea>
+        <ButtonArea>
+          <StyledButton>Sign In</StyledButton>
+        </ButtonArea>
+        <LinkArea>
+          <Link to="/signup">Sign up</Link>
+          <span>/</span>
+          <Link to="/forgot">find PW</Link>
+        </LinkArea>
+      </FormWrap>
+    );
+  }
+}
+
+export default SigninForm;
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "styled-components";
+import { Row } from "antd";
+import SigninForm from "../components/SigninForm";
+import signinBg from "../images/signin_bg4.jpg";
+
+const Page = styled(Row).attrs(() => ({
+  type: "flex",
+  align: "middle"
+}))`
+  height: 100vh;
+  background: url(${signinBg}) no-repeat center center;
+  background-size: cover;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+`;
+
+const Contents = styled.div`
+  position: absolute;
+  width: 700px;
+  top: 150px;
+  left: 50%;
+  margin-left: -350px;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+  line-height: 1;
+  text-shadow: 0 2px 10px rgba(255, 80, 155, 0.3);
+`;
+
+const SubTitle = styled.div`
+  text-align: center;
+  margin-top: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: rgba(255, 255, 255, 0.7);
+
+  &:after {
+    content: "";
+    display: block;
+    width: 200px;
+    height: 2px;
+    margin: 30px auto 20px;
+    background-color: #fff;
+  }
+`;
+
+function Signin() {
+  return (
+    <Page>
+      <Contents>
+        <Title>REVIEW SERVICE FOR BOOKS</Title>
+        <SubTitle>Plase share your opinion on web development books.</SubTitle>
+        <SigninForm />
+      </Contents>
+    </Page>
+  );
+}
+
+export default Signin;
